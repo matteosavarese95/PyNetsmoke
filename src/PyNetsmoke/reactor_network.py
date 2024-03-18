42772258375e5ccf97fc9e6b9692c1c807a85bec
Matteo Savarese <matteo.savarese@ulb.be>
2024-03-18 16:01:22 +0100

    Functions for CRN generation, no recirculation

--- src/PyNetsmoke/reactor_network.py
+++ src/PyNetsmoke/reactor_network.py
@@ -348,13 +348,14 @@ class ReactorNetwork:
 '''Class for generalized CRN'''
 class GeneralCRN:
 
-    def __init__(self, KinFile, Thermofile):
+    def __init__(self, KinFile, Thermofile, CanteraMech="gri30.cti"):
 
         self.Kinfile_ = KinFile
         self.Thermofile_ = Thermofile
+        self.CanteraMech_ = CanteraMech
 
     # Define function to initialize input layer
-    def InputLayer(self, Inputs):
+    def InputLayer(self, Inputs, taui=1e-6):
 
         # Number of inputs
         nin = len(Inputs)
@@ -362,9 +363,17 @@ class GeneralCRN:
         # Create each node as input node
         nodes = []
         locs  = []
+        # Initialize list of reference mixtures and parameters
+        ref_mixtures = []
+        param_list = []
         for i in range(nin):
             nodes.append(("I"+str(i),  "PSR"))
             locs.append((1, i))
+            # For inlets, the reference input is the Cantera quantity object
+            ref_mixtures.append(Inputs[i])
+            # Create the dictionary for standard inlet parameters
+            params = {'tau':taui, 'Mf':Inputs[i].mass, "volume":None}
+            param_list.append(params)
 
         # Create the attribute of layers
         self.layers_ = {"input":nodes}
@@ -372,10 +381,36 @@ class GeneralCRN:
         # Initialize connections
         self.connections_ = []
         self.splitting_ratios_ = []
+        # Initialze reference mixture and parameter list
+        self.ref_mixtures_ = ref_mixtures
+        self.param_list_ = param_list
 
         return self
     
-    def AddConnectedLayer(self, nr, rtype="PSR"):
+    def AddConnectedLayer(self, nr, rtype="PSR", params=None, param_list=None, 
+                          taui=1e-3, Li=1.0, Di=0.01, ref_mixture=None):
+
+        '''This function will add a fully cinnected layer of reactors.
+        The default reactor type is PSR, otherwise you can specify PFR via the
+        keyword rtype. The keyword params is a dictionary containing parameters that
+        will be used to initialize each reactor in the layer. Otherwise, if you want to
+        pass specific parameters to each reactor, you can do it using the list param_list,
+        which is a list of dictionaries, and each dictionary contains the parameters
+        for each reactor'''
+
+        # Check if params is None
+        if params == None and rtype == "PSR" and param_list==None:
+            pars = {'tau':taui, 'volume':None}
+            param_list = []
+            for i in range(nr):
+                param_list.append(pars)
+        elif params == None and rtype == "PFR" and param_list == None:
+            pars = {'L':Li, 'D':Di, 'volume':None}
+            param_list = []
+            for i in range(nr):
+                param_list.append(pars)
+
+        # If param_list is given, none of the two if willl be executed and param_list will be used
 
         # Count existing nodes
         total_nodes = sum(len(nodes) for nodes in self.layers_.values())
@@ -396,7 +431,7 @@ class GeneralCRN:
 
         newconns = []
         splitting_ratios = []
-        split_ratio = round(1/n_last, 4)
+        split_ratio = round(1/nr, 4)
         for i in range(n_last):
             for j in range(nr):
                 
@@ -426,10 +461,34 @@ class GeneralCRN:
         # Update splitting rations
         for item in splitting_ratios:
             self.splitting_ratios_.append(item)
+        # Update parameters list
+        for item in param_list:
+            self.param_list_.append(item)
 
+        # Update reference mixtures
+        if ref_mixture == None:
+            gas = ct.Solution(self.CanteraMech_)
+            gas.TPX = 300.0, 101325.0, "O2:0.21, N2:0.79"
+            ref_mixture = gas
+        for i in range(nr):
+            self.ref_mixtures_.append(ref_mixture)
+        
         return self
     
-    def AddOutputLayer(self, nr, rtype="PSR"):
+    def AddOutputLayer(self, nr, rtype="PSR", params=None, param_list=None, 
+                          taui=1e-3, Li=1.0, Di=0.01, ref_mixture=None):
+        
+        # Check if params is None
+        if params == None and rtype == "PSR" and param_list==None:
+            pars = {'tau':taui, 'volume':None}
+            param_list = []
+            for i in range(nr):
+                param_list.append(pars)
+        elif params == None and rtype == "PFR" and param_list == None:
+            pars = {'L':Li, 'D':Di, 'volume':None}
+            param_list = []
+            for i in range(nr):
+                param_list.append(pars)
 
         # Count existing nodes
         total_nodes = sum(len(nodes) for nodes in self.layers_.values())
@@ -444,7 +503,7 @@ class GeneralCRN:
 
         newconns = []
         splitting_ratios = []
-        split_ratio = round(1/n_last, 4)
+        split_ratio = round(1/nr, 4)
         for i in range(n_last):
             for j in range(nr):
                 
@@ -474,6 +533,17 @@ class GeneralCRN:
         # Update splitting rations
         for item in splitting_ratios:
             self.splitting_ratios_.append(item)
+        # Update parameters list
+        for item in param_list:
+            self.param_list_.append(item)
+
+        # Update reference mixtures
+        if ref_mixture == None:
+            gas = ct.Solution(self.CanteraMech_)
+            gas.TPX = 300.0, 101325.0, "O2:0.21, N2:0.79"
+            ref_mixture = gas
+        for i in range(nr):
+            self.ref_mixtures_.append(ref_mixture)
 
         return self
     
@@ -481,13 +551,15 @@ class GeneralCRN:
         '''This function creates the graph of the CRN using the networkX library'''
         G = nx.Graph()
         # Add the nodes
+        i=0
         for layer, nodes in self.layers_.items():
             for node, node_type in nodes:
                 if node_type == "PFR":
                     shape="box"
-                    G.add_node(node, type=node_type, shape=shape)
+                    G.add_node(node, type=node_type, shape=shape, ref_mixture=self.ref_mixtures_[i], parameters=self.param_list_[i], Rid=i)
                 else:
-                    G.add_node(node, type=node_type)
+                    G.add_node(node, type=node_type, ref_mixture=self.ref_mixtures_[i], parameters=self.param_list_[i], Rid=i)
+                i+=1
 
         # Add the connections
         for i in range(len(self.connections_)):
@@ -506,7 +578,187 @@ class GeneralCRN:
                 node_size=1500, edge_color='gray')
             plt.show()
 
-        return G
+        # Add graph to self
+        self.G_ = G
+
+        return self
+    
+    def GetMassFlowrates(self):
+
+        # Get total number of reactors
+        nr_tot = len(list(self.G_.nodes))
+        self.nr_tot_ = nr_tot
+
+        # Initialize the alpha matrix of splitting ratios
+        alpha = np.zeros((nr_tot, nr_tot))
+
+        # Assign split
+        # Assign mass flow rates based on reactor indices
+        for edge in self.G_.edges(data=True):
+            node1, node2, weight = edge[0], edge[1], edge[2]['weight']
+            Rid1 = self.G_.nodes[node1]['Rid']
+            Rid2 = self.G_.nodes[node2]['Rid']
+            alpha[Rid1, Rid2] = weight
+
+        # Create array of boundary conditions
+        bc = np.zeros(nr_tot)
+        for i, node in enumerate(self.G_.nodes):
+            if node.startswith('I'):
+                mass = self.G_.nodes[node]['ref_mixture'].mass
+                bc[i] = mass
+
+        # Solve the linear system
+        A = np.eye(nr_tot) - alpha.T
+        M = np.linalg.solve(A,bc)
+
+        mass_flowrates = np.zeros((nr_tot, nr_tot))
+        for i in range(len(M)):
+            mass_flowrates[i,:] = alpha[i,:] * M[i]
+
+        # Get the outlet mass flowrates
+        bc_out = np.zeros(self.nr_tot_)
+        for i, node in enumerate(self.G_.nodes):
+            if node.startswith('O'):
+                bc_out[i] = sum(mass_flowrates[:,i]) - sum(mass_flowrates[i,:])
+                if bc_out[i] < 0:
+                    raise ValueError("Negative outlet mass flowrate detected in reactor %d" % i)
+        
+
+        self.mass_flowrates_ = mass_flowrates
+        self.bc_in_ = bc
+        self.bc_out_ = bc_out
+
+        return self
+    
+    def CheckMassBalance(self):
+
+        reactor_in  = np.zeros(self.nr_tot_)
+        reactor_out = np.zeros(self.nr_tot_)
+        local_imbalance = np.zeros(self.nr_tot_)
+
+        # Mass balance of the network
+        for j in range(self.nr_tot_):
+            reactor_in[j]  = sum(self.mass_flowrates_[:,j])             # Mass inflow from other reactors
+            reactor_out[j] = sum(self.mass_flowrates_[j,:])             # Mass outflow towards other reactors
+            ext_input = self.bc_in_[j]                                  # Mass inflow from inlets
+            ext_output = self.bc_out_[j]                               # Mass inflow from outlets
+            inl = reactor_in[j] + ext_input                             # Total input
+            out = reactor_out[j] + ext_output                           # Total output
+            local_imbalance[j] = abs(inl-out)/max(inl,out)              # Local imbalance (relative %)
+
+        # Total input and total output in the network
+        total_in  = sum(reactor_in)
+        total_out = sum(reactor_out)
+
+        global_imbalance = abs(total_in - total_out)/max(total_in, total_out)
+
+        # Check if the mass balance is globally satisfied
+        mass_check = True
+        if global_imbalance > 1e-12:
+            raise ValueError('Check internal consistency of the network')
+        
+        else:
+            # Check if the local mass balance is satisfied
+            for j in range(self.nr_tot_):
+                if local_imbalance[j] > 0.01: 
+                    mass_check = False
+                    stri = "Mass balance over 1 percent detected in reactor " + str(j)
+                    raise ValueError(stri)
+
+        if mass_check == True:
+            print("Global mass imbalance and local mass imbalance within tolerances")
+            print("Global imbalance = ", global_imbalance*100, ' percent')
+
+        return self
+
+    def WriteInputs(self):
+
+        # Initialize reactor list
+        rlist = []
+
+        # Iterate over the nodes of the CRN to define the single reactors
+        for i, node in enumerate(self.G_.nodes):
+            rtype = self.G_.nodes[node]['type']
+            parameters = self.G_.nodes[node]['parameters']
+            mixture = self.G_.nodes[node]['ref_mixture']
+            rid = self.G_.nodes[node]['Rid']
+
+            # Get mass flowrate
+            Mf = sum(self.mass_flowrates_[:,i])
+
+            # Create reactor object
+            if rtype == "PSR":
+                isinput = False
+                isoutput = False
+                # Check if reactor is input or output
+                if node.startswith('I'):
+                    isinput=True
+                    # Get mass flowrate
+                    Mf = mixture.mass
+                if node.startswith('O'):
+                    isoutput=True 
+
+                # Check if volume is a parameter of parameters
+                if parameters["volume"] == None:
+                    r = Reactor(Rtype=rtype, tau=parameters["tau"], Mf=Mf, CanteraMech=self.CanteraMech_, InletMixture=mixture,
+                                InitialStatus=mixture, isinput=isinput, isoutput=isoutput)
+                else:
+                    r = Reactor(Rtype=rtype, volume=parameters["volume"], Mf=Mf, CanteraMech=self.CanteraMech_, InletMixture=mixture,
+                                InitialStatus=mixture, isinput=isinput, isoutput=isoutput)
+            # Create PFR reactor
+            elif rtype == "PFR":
+
+                isinput = False
+                isoutput = False
+                # Check if reactor is input or output
+                if node.startswith('I'):
+                    isinput=True
+                if node.startswith('O'):
+                    isoutput=True
+
+                r = Reactor(Rtype=rtype, L=parameters["L"], D=parameters["D"], Mf=Mf, CanteraMech=self.CanteraMech_, InletMixture=mixture,
+                                InitialStatus=mixture, isinput=isinput, isoutput=isoutput)
+                
+            # Append reactor to list
+            rlist.append(r)
+
+        # Create reactor network object
+        rn = ReactorNetwork(rlist, self.mass_flowrates_, KinFile=self.Kinfile_, ThermoFile=self.Thermofile_, CanteraMech=self.CanteraMech_)
+        rn.WriteNetworkInput()
+
+        # Add network to object
+        self.rn_ = rn
+
+        return self
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+        
+
+        
+
+
+
+
+
+
 
 
         
